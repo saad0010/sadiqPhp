8bb5a7d148c5e2f3958467c58e1e4a7cc7ce2c13
Saad <118393620+saad0010@users.noreply.github.com>
2023-09-11 02:56:51 +0500

    Revised Version

---
+++ README.md
@@ -0,0 +1,15 @@
+https://youtu.be/st7ooWcjl_U?t=141
+
+php save
+
+https://youtu.be/v4rwyroyM5Y?t=201
+
+clean code
+make mobile friendly -- Done
+email send on clicks,
+
+set banner text -- Done
+remove ul list default style -- Done
+table style : https://www.youtube.com/watch?v=Ay8BXbAmEYM
+show header in colors
+ico
---
+++ patches.php
@@ -0,0 +1,66 @@
+<?php include 'includes/header.php'; ?>
+
+<section class="midd-w3 py-5" id="faq">
+    <div class="container py-xl-5 py-lg-3">
+        <div class="row">
+            <div class="col-lg-6 about-right-faq">
+                <h6>Services</h6>
+                <h3 class="text-da">Patches </h3>
+                <p class="mt-4">We have professional and experienced embroidery digitizers for converting your designs
+                    into the digitized embroidery file format of your choice.</p>
+                <ul class="w3l-right-book mt-4">
+                    <li>Flat Embroidery </li>
+                    <li>Applique Embroidery </li>
+                    <li>3D / Puff Embroidery </li>
+                    <li>Chenille Embroidery </li>
+
+                </ul>
+                <a href="contact.php" class="btn button-style button-style-2 mt-sm-5 mt-4">Contact US</a>
+            </div>
+            <div class="col-lg-6 left-wthree-img text-right">
+                <a href="images/m1r.jpg" class="hov" data-lightbox="models" data-title="Caption1">
+                    <h4 class="textHover ">Click To Enlarge </h4>
+                    <img class="img-fluid ob   mt-3" style="border-radius: 2rem; " src="images/m1r.jpg" alt="">
+
+                </a>
+                <!-- <img src="images/p1.jpg" alt="" class="img-fluid mt-5" /> -->
+            </div>
+            <div class="row col-lg-12 hed">
+                <h4>Samples</h4>
+            </div>
+
+            <div class="container2">
+                <div class="gallery">
+                    <a class="hov" href="images/p2r.jpg" data-lightbox="models" data-title="Caption1">
+                        <h4 class="textHover1 ">Click To Enlarge </h4>
+                        <img src="images/p2rs.jpg" alt="">
+                    </a>
+
+                    <a class="hov" href="images/p3r.jpg" data-lightbox="models" data-title="Caption2">
+                        <h4 class="textHover1 ">Click To Enlarge </h4>
+                        <img src="images/p3rs.jpg" alt="">
+                    </a>
+                    <a class="hov" href="images/p4r.jpg" data-lightbox="models" data-title="Caption3">
+                        <h4 class="textHover1 ">Click To Enlarge </h4>
+                        <img src="images/p4rs.jpg" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- The Modal -->
+    <div id="myModal" class="modal">
+
+        <!-- The Close Button -->
+        <span class="close">&times;</span>
+
+        <!-- Modal Content (The Image) -->
+        <img class="modal-content" id="img01">
+
+        <!-- Modal Caption (Image Text) -->
+        <div id="caption"></div>
+    </div>
+</section>
+
+<?php include 'includes/footer.php'; ?>
